88610a8c1a30e53faa949df8248486ed12702339
jackson <jw.jacksonwang28@gmail.com>
2023-12-22 09:22:16 +0800

    :q

--- config.sh
+++ config.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 echo "start config"
-./neovim/config.sh
+if [ "$1" -eq 1 ]; then
+    ./neovimNoNerdFont/config.sh
+else
+    ./neovim/config.sh
+fi
 ./tmux/config.sh
 echo "end config"
+
 
 
 
